b42a6cbb91b83b7262670b61f382591c3b77831d
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-22 19:47:48 +0900

    Feat: 물주기 UI & 레이아웃

---
+++ src/app/water/page.tsx
@@ -0,0 +1,152 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="flex h-[932px] items-center justify-around px-3 py-[79px] relative bg-[#f9f9f9] w-full min-w-[430px]">
+      <div className="flex flex-col w-full max-w-[400px] items-start justify-center gap-14 px-4 py-0 relative">
+        <div className="flex items-center justify-between relative w-full">
+          <p className="text-[28px] font-bold text-[#12121d] leading-7">
+            <span className="tracking-[-0.08px]">Water </span>
+            <span className="text-[#5b8e55] tracking-[-0.08px]">Plants</span>
+          </p>
+
+          {/* <VuesaxLinearSetting2 className="w-8 h-8" /> */}
+        </div>
+
+        <div className="flex flex-col h-[113.98px] items-start gap-2.5 px-[18px] py-[15px] bg-[#e6bdbb] rounded-[20px] w-full">
+          <div className="inline-flex items-center gap-6">
+            <img
+              className="w-[91.29px] h-[83.98px] object-cover"
+              alt="Element plants png"
+              src="https://c.animaapp.com/m8k070m1mBEnyl/img/57-572798-plants-png-2.png"
+            />
+
+            <div className="w-[120.45px] h-[66px] relative">
+              <div className="absolute w-[120px] top-0 left-0 font-medium text-white text-base">
+                Create with Gardening App
+              </div>
+
+              <div className="inline-flex items-center gap-1.5 absolute top-[45px] left-0">
+                <img
+                  className="w-[11.24px] h-[13.52px] ml-[-0.75px]"
+                  alt="Vector"
+                  src="https://c.animaapp.com/m8k070m1mBEnyl/img/vector.svg"
+                />
+                <div className="text-white text-sm">150 ml</div>
+              </div>
+            </div>
+
+            <img
+              className="w-[82.73px] h-[82.73px] mb-[-3.37px] mr-[-13.00px]"
+              alt="Frame"
+              src="https://c.animaapp.com/m8k070m1mBEnyl/img/frame-26956.svg"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col h-[113.98px] items-start gap-2.5 px-[18px] py-3 bg-[#e6c2a0] rounded-[20px] w-full">
+          <div className="inline-flex items-center gap-6">
+            <img
+              className="w-[89.34px] h-[89.34px] object-cover"
+              alt="Element eke"
+              src="https://c.animaapp.com/m8k070m1mBEnyl/img/11232131312312-eke-1.png"
+            />
+
+            <div className="w-[120.45px] h-[66px] relative">
+              <div className="absolute w-[120px] top-0 left-0 font-medium text-white text-base">
+                Create with Gardening App
+              </div>
+
+              <div className="inline-flex items-center gap-1.5 absolute top-[45px] left-0">
+                <img
+                  className="w-[11.24px] h-[13.52px] ml-[-0.75px]"
+                  alt="Vector"
+                  src="https://c.animaapp.com/m8k070m1mBEnyl/img/vector.svg"
+                />
+                <div className="text-white text-sm">150 ml</div>
+              </div>
+            </div>
+
+            <img
+              className="w-[82.73px] h-[82.73px] mb-[-0.69px] mr-[-13.00px]"
+              alt="Frame"
+              src="https://c.animaapp.com/m8k070m1mBEnyl/img/frame-26965.svg"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col h-[117.98px] items-start gap-2.5 px-[29px] py-2 bg-[#dfdfdf] rounded-[20px] w-full border-2 border-[#ffa7a7]">
+          <div className="inline-flex items-center gap-[35px]">
+            <img
+              className="w-[47.14px] h-[91.01px] object-cover"
+              alt="Artshare ru plants"
+              src="https://c.animaapp.com/m8k070m1mBEnyl/img/artshare-ru-plants-28-1.png"
+            />
+
+            <div className="flex flex-col w-[120.45px] items-start gap-[3px] relative">
+              <div className="font-medium text-white text-base">
+                Create with Gardening App
+              </div>
+
+              <div className="inline-flex items-center gap-1.5">
+                <img
+                  className="w-[11.24px] h-[13.52px] ml-[-0.75px]"
+                  alt="Vector"
+                  src="https://c.animaapp.com/m8k070m1mBEnyl/img/vector.svg"
+                />
+                <div className="text-[#969c96] text-sm">150 ml</div>
+              </div>
+            </div>
+
+            <div className="flex w-[56.73px] h-[56.73px] items-center gap-2.5 px-5 py-[18px] bg-[#ffffff69] rounded-[28.36px] shadow-[0px_4px_15px_-2px_#0000000d] backdrop-blur-[6px]">
+              <img
+                className="w-[18.45px] h-[22.31px]"
+                alt="Group"
+                src="https://c.animaapp.com/m8k070m1mBEnyl/img/group-49.png"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-start gap-[17px] w-full">
+          <div className="text-[27.8px] font-medium text-black">
+            Saturday, May 21
+          </div>
+
+          <div className="flex flex-col h-[113.98px] items-start gap-2.5 px-[27px] py-2 bg-[#dfdfdf] rounded-[20px] w-full">
+            <div className="inline-flex items-center gap-[29px]">
+              <img
+                className="w-[72.13px] h-[83.57px] object-cover"
+                alt="Element potted"
+                src="https://c.animaapp.com/m8k070m1mBEnyl/img/144-1444325-potted-plants-clipart-transparent-background-ikea-pl.png"
+              />
+
+              <div className="flex flex-col w-[120.45px] items-start gap-[3px] relative">
+                <div className="font-medium text-white text-base">
+                  Create with Gardening App
+                </div>
+
+                <div className="inline-flex items-center gap-1.5">
+                  <img
+                    className="w-[11.24px] h-[13.52px] ml-[-0.75px]"
+                    alt="Vector"
+                    src="https://c.animaapp.com/m8k070m1mBEnyl/img/vector.svg"
+                  />
+                  <div className="text-[#969c96] text-sm">150 ml</div>
+                </div>
+              </div>
+
+              <div className="flex w-[56.73px] h-[56.73px] items-center gap-2.5 px-5 py-[18px] bg-[#ffffff69] rounded-[28.36px] shadow-[0px_4px_15px_-2px_#0000000d] backdrop-blur-[6px]">
+                <img
+                  className="w-[18.45px] h-[22.31px]"
+                  alt="Group"
+                  src="https://c.animaapp.com/m8k070m1mBEnyl/img/group-50.png"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
