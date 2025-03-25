726dba3f6a0ec5603b4842a0c024e9a53ff2a20c
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-25 21:05:53 +0900

    Refactor: 컴포넌트 분리

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,33 +1,30 @@
 import React from "react";
+import BackgroundImage from "../_components/BackgroundImage";
+import Header from "../_components/Header";
+import Button from "../_components/Button";
 
 export default function Page() {
   return (
-    <div className="relative flex min-h-screen items-center justify-center bg-gradient-to-b from-transparent to-[#152b0a] text-[#24570b]">
+    <div className="relative flex min-h-screen items-center justify-center ">
       {/* 배경 이미지 */}
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{
-          backgroundImage:
-            "url(https://c.animaapp.com/m8jyupbtBv3Vrq/img/-welcome.png)",
-        }}
+      <BackgroundImage
+        src={"/images/welcome-bg-01.webp"}
+        overlay={true}
+        opacity={20}
       />
 
       {/* Contents */}
-      <div className="relative z-10 flex flex-col justify-center items-center w-full max-w-md px-6 py-10 bg-white/10 backdrop-blur-xl rounded-2xl shadow-lg min-h-[70vh]">
+      <div className="relative z-10 flex flex-col justify-center items-center w-full max-w-md px-6 py-10 bg-white/10 backdrop-blur-xl rounded-2xl shadow-lg min-h-[80vh]">
         {/* 헤더 */}
-        <div className="flex items-center gap-3 mb-6">
-          <h1 className="text-[#24570b] text-4xl font-semibold">
-            RagdollPlants
-          </h1>
-        </div>
+        <Header mainText="RagdollPlants" subText="랙돌플랜츠" />
 
         {/* Paragraph */}
         <div className="flex flex-col text-center mb-8 pt-8 gap-y-8">
-          <h2 className="text-3xl font-medium text-[#24570b] mb-2 flex flex-col">
+          <h2 className="text-3xl   mb-2 flex flex-col text-white">
             <span>랙돌플랜츠에 오신 것을</span>
             <span>환영합니다!</span>
           </h2>
-          <p className="text-base text-[#24570b] leading-relaxed">
+          <p className="text-base  leading-relaxed text-white">
             식물들과 함께 프레시함을 느껴요.
             <br />
             삶의 공간을 넓혀보세요!
@@ -36,12 +33,9 @@ export default function Page() {
 
         {/* 버튼 */}
         <div className="flex flex-col gap-4 w-full max-w-xs">
-          <button className="h-12 w-full text-lg font-medium text-white bg-green-600 rounded-lg hover:bg-green-700 transition">
-            로그인
-          </button>
-          <button className="h-12 w-full text-lg font-medium bg-white border-green-600 border text-green-600 rounded-lg hover:bg-[#f4f4f4] transition">
-            회원가입
-          </button>
+          <Button text="로그인" type="primary" />
+          <Button text="Login With Google" />
+          <Button text="회원가입" />
         </div>
       </div>
     </div>
