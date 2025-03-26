1f6e2992c6ca5bb3aa520bc15eb3ca280babafde
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-26 21:12:48 +0900

    Chore: ts eslint 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // 사용되지 않는 변수에 대한 경고를 끄는 규칙 추가
+    },
+  },
 ];
 
 export default eslintConfig;
