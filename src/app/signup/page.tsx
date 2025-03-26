e12cd813147f5fb7026c581e8c6ee8ab3ac90e80
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-26 16:20:14 +0900

    Refactor: 컴포넌트 분리 및 최적화

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,64 +1,47 @@
 import React from "react";
+import ContentLayout from "../_components/ContentsLayout";
+import BackgroundImage from "../_components/BackgroundImage";
+import Header from "../_components/Header";
+import { Input } from "../_components/Input";
+import { Checkbox } from "../_components/Checkbox";
+import { Button } from "../_components/Button";
+import { PasswordInput } from "../_components/PasswordInput";
 
 export default function Page() {
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen w-full px-6 py-12 bg-white">
+    <>
       {/* Contents */}
-      <div className="relative z-10 flex flex-col items-start gap-6 w-full max-w-md px-8 py-12 bg-white/10 backdrop-blur-xl rounded-2xl shadow-lg">
+      <BackgroundImage src={"/images/welcome-bg-01.webp"} overlay={true} />
+
+      <ContentLayout>
         {/* 헤더 */}
-        <h1 className="text-3xl font-semibold text-[#24570b]">
-          RagdollPlants
-        </h1>
-        <h2 className="text-xl font-semibold text-gray-900">
-          회원가입 on <span className="text-green-600">랙돌플랜츠</span>
-        </h2>
-        <p className="text-gray-600 text-sm">
-          계정을 생성하세요. 식물들이 기다리고 있어요!
-        </p>
+        <Header mainText="RagdollPlants" subText="랙돌플랜츠" />
+
+        {/* Paragraph*/}
+        <div className="flex flex-col text-center mb-8 pt-8 text-white">
+          <p className="tracking-wide ">
+            계정을 생성하세요. 식물들이 기다리고 있어요!
+          </p>
+        </div>
 
         {/* 입력 폼 */}
-        <input
-          type="text"
-          placeholder="Name"
-          className="w-full p-3 bg-gray-100 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600"
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full p-3 bg-gray-100 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600"
-        />
-        <input
-          type="text"
-          placeholder="Phone"
-          className="w-full p-3 bg-gray-100 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full p-3 bg-gray-100 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600"
-        />
-        <input
-          type="password"
-          placeholder="Repeat Password"
-          className="w-full p-3 bg-gray-100 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600"
-        />
+        <div className="flex flex-col gap-4 w-full">
+          <Input placeholder="아이디" type="text" />
+          <Input placeholder="이름" type="text" />
+          <Input placeholder="이메일" type="email" />
+          <Input placeholder="연락처 (선택사항)" type="text" />
+          <PasswordInput placeholder="비밀번호" />
+          <PasswordInput placeholder="비밀번호 재확인" />
+        </div>
 
         {/* 이용 약관 */}
-        <div className="flex items-center gap-2">
-          <input
-            type="checkbox"
-            className="w-5 h-5 text-green-600 border-gray-300 rounded focus:ring-2 focus:ring-green-600"
-          />
-          <span className="text-sm text-gray-600">
-            I Agree to the terms and conditions
-          </span>
+        <div className="w-full py-4">
+          <Checkbox label="이용약관에 모두 동의합니다" />
         </div>
 
         {/* 버튼 */}
-        <button className="w-full py-3 text-white bg-green-600 rounded-lg hover:bg-green-700 transition font-medium">
-          회원가입
-        </button>
-      </div>
-    </div>
+        <Button text="회원가입" type="primary" />
+      </ContentLayout>
+    </>
   );
 }
