6d2b31c0236640724a680569d51f06175f324efe
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-26 16:19:14 +0900

    Style: 기본 스타일 변경

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -5,11 +5,11 @@ type ButtonProps = {
   type?: "normal" | "primary" | "outlined"; // 추가적인 버튼 타입 지원
 };
 
-function Button({ text, type = "normal" }: ButtonProps) {
+export const Button = ({ text, type = "normal" }: ButtonProps) => {
   const baseStyle = "h-12 w-full text-lg rounded-lg transition";
   const primaryStyle = "bg-green-600 text-white hover:bg-green-700 border-none";
   const normalStyle =
-    "bg-white text-green-600 hover:bg-gray-200 border-green-600 border";
+    "bg-parent text-white hover:text-green-500 hover:border-green-600 border-white border-2";
   let buttonStyle = "";
 
   switch (type) {
@@ -25,6 +25,4 @@ function Button({ text, type = "normal" }: ButtonProps) {
   }
 
   return <button className={`${baseStyle} ${buttonStyle}`}>{text}</button>;
-}
-
-export default Button;
+};
