9fad0f103e037853141b3276b16f4d3ba33a2fc0
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-26 16:19:34 +0900

    Feat: 컨텐츠 레이아웃

---
+++ src/app/_components/ContentsLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ContentLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative z-10 flex flex-col justify-center items-center w-full max-w-md px-10 py-10 bg-white/10 backdrop-blur-xl rounded-2xl shadow-lg min-h-[80vh] ">
+      {children}
+    </div>
+  );
+}
