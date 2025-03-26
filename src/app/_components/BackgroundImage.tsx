4552168de4600edfc24ea48a6ac292be748dab42
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-26 16:18:59 +0900

    Style: 이미지 레거시 스타일 제거

--- src/app/_components/BackgroundImage.tsx
+++ src/app/_components/BackgroundImage.tsx
@@ -4,24 +4,28 @@ import React from "react";
 type BackgroundImageProps = {
   src: string;
   overlay?: boolean;
-  opacity?: number;
 };
 
 export const BackgroundImage = ({
   src,
   overlay = false,
-  opacity = 0,
 }: BackgroundImageProps) => {
   return (
     <div>
       {/* 배경 이미지 */}
       <div className="absolute inset-0 z-0">
-        <Image alt="background" src={src} fill priority objectFit="cover" />
+        <Image
+          alt="background"
+          src={src}
+          fill
+          priority
+          style={{ objectFit: "cover" }}
+        />
       </div>
 
       {/* 어두운 오버레이 */}
       {overlay && (
-        <div className={`absolute inset-0 bg-black opacity-${opacity} z-0`} />
+        <div className={`absolute inset-0 bg-black opacity-20 z-0`} />
       )}
     </div>
   );
