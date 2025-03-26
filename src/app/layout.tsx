c4661d5dc214961619a80c9429c7e970af193389
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-26 16:18:17 +0900

    Style: 루트 레이아웃 스타일

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="relative flex min-h-screen items-center justify-center">
+          {children}
+        </div>
       </body>
     </html>
   );
