19076783999d5b2a9d8bddc1b18ab64abb7e8eb7
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-27 19:22:44 +0900

    Refactor: svg 컴포넌트화 Style: 레이아웃 조정

--- src/app/_components/MenuList.tsx
+++ src/app/_components/MenuList.tsx
@@ -1,46 +1,56 @@
 import React from "react";
 import Water from "@/../public/svg/water.svg";
 import Plant from "@/../public/svg/plant.svg";
+import Link from "next/link";
 
 const menuItems = [
   {
-    label: "My Garden",
-    icon: Water,
+    label: "내 정원",
+    icon: Plant,
+    link: "/mygarden",
   },
   {
-    label: "My Plants",
+    label: "내 식물",
     icon: Plant,
+    link: "/myplants",
   },
   {
-    label: "Example",
+    label: "내 앨범",
     icon: Plant,
+    link: "/",
   },
   {
-    label: "Example2",
+    label: "내 게시글",
     icon: Plant,
+    link: "/",
   },
 ];
 
 export const MenuList = () => {
   return (
-    <div className="flex items-center gap-6 mt-6 w-full">
-      {menuItems.map((item, index) => (
-        <div key={index} className="flex flex-col items-center gap-2 w-20">
-          <div className="w-14 h-14 bg-[#5b8e55] rounded-full shadow-lg flex items-center justify-center relative">
-            {item.icon && (
-              <div className="relative size-8 ">
-                <item.icon
-                  className="w-full h-full"
-                  style={{ stroke: "#ff0000" }}
-                />
+    <div className="flex items-center gap-4 py-4 w-full">
+      {menuItems.map((item) => {
+        const Icon = item.icon;
+        return (
+          <Link href={`${item.link}`} key={item.label}>
+            <div className="flex flex-col items-center gap-2 w-16">
+              <div className="w-14 h-14 bg-[#5b8e55] rounded-full shadow-lg flex items-center justify-center relative">
+                {Icon && (
+                  <div className="relative size-8 ">
+                    <Icon
+                      className="w-full h-full"
+                      style={{ stroke: "#ff0000" }}
+                    />
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-          <p className="text-[#5b8e55] text-xs font-medium text-center">
-            {item.label}
-          </p>
-        </div>
-      ))}
+              <p className="text-[#5b8e55] text-xs font-semibold text-center">
+                {item.label}
+              </p>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
