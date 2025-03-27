fb8618a1c1b3796956bd136715255b7fe7962ca7
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-27 19:24:47 +0900

    Style: 상하정렬 제거

--- src/app/_components/ContentsLayout.tsx
+++ src/app/_components/ContentsLayout.tsx
@@ -6,7 +6,7 @@ export default function ContentLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative z-10 flex flex-col justify-center items-center w-full max-w-md px-10 py-10 my-10 bg-white/10 backdrop-blur-xl rounded-2xl shadow-lg min-h-[80vh] ">
+    <div className="relative z-10 flex flex-col items-center w-full max-w-md px-10 py-10 my-10 bg-white/10 backdrop-blur-xl rounded-2xl shadow-lg min-h-[80vh] ">
       {children}
     </div>
   );
