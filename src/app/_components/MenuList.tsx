76827967ac67d16e31454db21820c2596cfc8dde
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-28 18:53:34 +0900

    Style: 크기 조정

--- src/app/_components/MenuList.tsx
+++ src/app/_components/MenuList.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Water from "@/../public/svg/water.svg";
 import Plant from "@/../public/svg/plant.svg";
+import Garden from "@/../public/svg/garden.svg";
 import Link from "next/link";
 
 const menuItems = [
   {
     label: "내 정원",
-    icon: Plant,
+    icon: Garden,
     link: "/mygarden",
   },
   {
@@ -15,9 +16,9 @@ const menuItems = [
     link: "/myplants",
   },
   {
-    label: "내 앨범",
-    icon: Plant,
-    link: "/",
+    label: "물주기",
+    icon: Water,
+    link: "/water",
   },
   {
     label: "내 게시글",
@@ -34,17 +35,17 @@ export const MenuList = () => {
         return (
           <Link href={`${item.link}`} key={item.label}>
             <div className="flex flex-col items-center gap-2 w-16">
-              <div className="w-14 h-14 bg-[#5b8e55] rounded-full shadow-lg flex items-center justify-center relative">
+              <div className="w-14 h-14 bg-[#73c06b] rounded-full shadow-lg flex items-center justify-center relative">
                 {Icon && (
                   <div className="relative size-8 ">
                     <Icon
                       className="w-full h-full"
-                      style={{ stroke: "#ff0000" }}
+                      style={{ stroke: "white", }}
                     />
                   </div>
                 )}
               </div>
-              <p className="text-[#5b8e55] text-xs font-semibold text-center">
+              <p className="text-[#8ee984] text-xs font-semibold text-center">
                 {item.label}
               </p>
             </div>
