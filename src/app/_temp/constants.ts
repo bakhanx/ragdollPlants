3d612b6c9d876f0a509f114ccd2a70f617c1f4d2
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-30 21:07:15 +0900

    Feat: 임시 데이터

---
+++ src/app/_temp/constants.ts
@@ -0,0 +1,77 @@
+
+export const plantItems = [
+  {
+    name: "Cactus",
+    image: "/images/welcome-bg-01.webp",
+    isNew: true,
+  },
+  {
+    name: "Cactus Red",
+    image: "/images/welcome-bg-02.webp",
+    isNew: false,
+  },
+  {
+    name: "Unknown Plant",
+    image: "/images/welcome-bg-03.webp",
+    isNew: false,
+  },
+  // {
+  //   name: "선인장",
+  //   image: "/images/welcome-bg-04.webp",
+  //   isNew: true,
+  // },
+];
+export const myPlants = [
+  {
+    name: "Cactus",
+    image: "/images/welcome-bg-01.webp",
+    isNew: true,
+    status: true
+  },
+  {
+    name: "Cactus Red",
+    image: "/images/welcome-bg-02.webp",
+    isNew: false,
+    status: true
+  },
+  {
+    name: "Unknown Plant",
+    image: "/images/welcome-bg-03.webp",
+    isNew: false,
+    status: false
+  },
+  {
+    name: "선인장",
+    image: "/images/welcome-bg-04.webp",
+    isNew: true,
+    status: false,
+  },
+  {
+    name: "Cactus",
+    image: "/images/welcome-bg-01.webp",
+    isNew: true,
+    status: true
+  },
+  {
+    name: "Cactus Red",
+    image: "/images/welcome-bg-02.webp",
+    isNew: false,
+    status: true
+  },
+  {
+    name: "Unknown Plant",
+    image: "/images/welcome-bg-03.webp",
+    isNew: false,
+    status: false
+  },
+  {
+    name: "선인장",
+    image: "/images/welcome-bg-04.webp",
+    isNew: true,
+    status: false,
+  },
+];
+
+
+
+export const profileImg = "/images/Profile.png";
