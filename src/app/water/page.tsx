5617c4fc10278ac65992e778e1d3a3bc446a9cc2
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-31 18:10:06 +0900

    Feat: 컴포넌트 분리

--- src/app/water/page.tsx
+++ src/app/water/page.tsx
@@ -1,44 +1,5 @@
 import React from "react";
-
-type WaterCardProps = {
-  backgroundColor: string;
-  imageSrc: string;
-  title: string;
-  amount: number;
-  statusIcon?: string;
-};
-
-const WaterCard = ({
-  backgroundColor,
-  imageSrc,
-  title,
-  amount,
-  statusIcon,
-}: WaterCardProps) => {
-  return (
-    <div
-      className={`flex items-center gap-4 p-4 rounded-xl w-full ${backgroundColor}`}
-    >
-      <img className="w-20 h-20 object-cover" alt="Plant" src={imageSrc} />
-      <div className="flex flex-col">
-        <p className="text-white font-medium">{title}</p>
-        <div className="flex items-center gap-2 mt-2">
-          <img
-            className="w-3 h-3"
-            alt="Vector"
-            src="https://c.animaapp.com/m8k070m1mBEnyl/img/vector.svg"
-          />
-          <p className="text-white text-sm">{amount}ml</p>
-        </div>
-      </div>
-      {statusIcon && (
-        <div className="flex items-center justify-center w-12 h-12 bg-white/30 rounded-full backdrop-blur-sm">
-          <img className="w-5 h-6" alt="Status Icon" src={statusIcon} />
-        </div>
-      )}
-    </div>
-  );
-};
+import { WaterCard } from "./_components/WaterCard";
 
 export default function Page() {
   return (
@@ -55,26 +16,26 @@ export default function Page() {
         {/* 카드 */}
         <WaterCard
           backgroundColor="bg-[#e6bdbb]"
-          imageSrc="https://c.animaapp.com/m8k070m1mBEnyl/img/57-572798-plants-png-2.png"
-          statusIcon="https://c.animaapp.com/m8k070m1mBEnyl/img/group-49.png"
+          imageSrc=""
+          statusIcon=""
           title="Create with Gardening App"
           amount={150}
         />
 
         <WaterCard
           backgroundColor="bg-[#e6c2a0]"
-          imageSrc="https://c.animaapp.com/m8k070m1mBEnyl/img/11232131312312-eke-1.png"
-          statusIcon="https://c.animaapp.com/m8k070m1mBEnyl/img/group-49.png"
+          imageSrc=""
+          statusIcon=""
           title="Create with Gardening App"
           amount={150}
         />
 
         <WaterCard
-          backgroundColor="bg-[#dfdfdf] border-2 border-red-400"
-          imageSrc="https://c.animaapp.com/m8k070m1mBEnyl/img/artshare-ru-plants-28-1.png"
+          backgroundColor="bg-[#dfdfdf]"
+          imageSrc=""
           title="Create with Gardening App"
           amount={150}
-          statusIcon="https://c.animaapp.com/m8k070m1mBEnyl/img/group-49.png"
+          statusIcon=""
         />
 
         {/* 지난 날 카드 */}
@@ -82,10 +43,10 @@ export default function Page() {
           <p className="text-2xl font-medium text-black">Saturday, May 21</p>
           <WaterCard
             backgroundColor="bg-[#dfdfdf]"
-            imageSrc="https://c.animaapp.com/m8k070m1mBEnyl/img/144-1444325-potted-plants-clipart-transparent-background-ikea-pl.png"
+            imageSrc=""
             title="Create with Gardening App"
             amount={150}
-            statusIcon="https://c.animaapp.com/m8k070m1mBEnyl/img/group-50.png"
+            statusIcon=""
           />
         </div>
       </div>
