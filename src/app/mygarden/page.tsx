f69a72d0932c5f5ea3f48fe5078166c3dfd5ae09
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-02 20:41:21 +0900

    Refactor: 컴포넌트 분리 Feat: 유저프로필 추가

--- src/app/mygarden/page.tsx
+++ src/app/mygarden/page.tsx
@@ -4,79 +4,29 @@ import ContentLayout from "../_components/ContentsLayout";
 import Header from "../_components/Header";
 import { MenuList } from "../_components/MenuList";
 import { PlantList } from "../_components/PlantList";
-import { plantItems } from "../_temp/constants";
 import PlantTitle from "./_components/PlantTitle";
+import UserProfile from "../_components/UserProfile";
+import { plantItems, userProfileData } from "../_temp/constants";
 
 export default function Page() {
+  const userData = userProfileData;
   return (
     <>
       <BackgroundImage src="/images/welcome-bg-06.webp" />
-
-      {/* Contents */}
       <ContentLayout>
-        {/* 헤더 */}
-        <Header />ㄹ{/* 메뉴 리스트 */}
+        <Header />
         <MenuList />
-        {/* 칭호 */}
-        <PlantTitle />
-        {/* 내 식물 목록 (대표사진)*/}
+
+        <UserProfile
+          nickname={userData.nickname}
+          level={userData.level}
+          stats={userData.stats}
+          levelProgress={userData.levelProgress}
+          todayWaterCount={userData.plantCare.waterCount}
+          nutrientCount={userData.plantCare.nutrientCount}
+        />
+
         <PlantList items={plantItems} />
-        {/* 유저 데이터 */}
-        <section className="w-full py-2 my-2 rounded-md relative">
-          <div className="absolute inset-0 bg-white opacity-30 rounded-md" />
-
-          <div className="relative text-sm px-2">
-            <div>팔로워 : 27</div>
-            <div>방문자 수 : 12</div>
-            <div>보유 식물수 : 30</div>
-          </div>
-          <div></div>
-        </section>
-        {/* 유저 데이터 */}
-        <section className="w-full py-2 my-2 rounded-md relative">
-          <div className="absolute inset-0 bg-white opacity-30 rounded-md" />
-
-          <div className="relative text-sm px-2">
-            <div>팔로워 : 27</div>
-            <div>방문자 수 : 12</div>
-            <div>보유 식물수 : 30</div>
-          </div>
-          <div></div>
-        </section>
-        {/* 유저 데이터 */}
-        <section className="w-full py-2 my-2 rounded-md relative">
-          <div className="absolute inset-0 bg-white opacity-30 rounded-md" />
-
-          <div className="relative text-sm px-2">
-            <div>팔로워 : 27</div>
-            <div>방문자 수 : 12</div>
-            <div>보유 식물수 : 30</div>
-          </div>
-          <div></div>
-        </section>
-        {/* 식물 상태 */}
-        {/* {["Cactus Red", "Cactus Green"].map((plant) => (
-          <div
-            key={plant}
-            className="w-full bg-white p-4 rounded-lg shadow-md flex items-center gap-4 backdrop-blur-md hover:scale-105 transition-all duration-300"
-          >
-            <img
-              className="w-24 sm:w-28 h-20 sm:h-24 object-cover rounded-md"
-              alt={plant}
-              src="https://c.animaapp.com/m8jzqjrxFYxxY8/img/rectangle-14.svg"
-            />
-            <div>
-              <h3 className="text-lg sm:text-xl font-medium">{plant}</h3>
-              <p className="text-sm sm:text-base text-gray-500">
-                Create an Aepod
-              </p>
-              <p className="text-sm sm:text-base text-[#61b458]">Water Level</p>
-              <div className="w-full bg-gray-200 h-1 rounded-full mt-1">
-                <div className="bg-[#60b357] h-1 rounded-full w-1/4"></div>
-              </div>
-            </div>
-          </div>
-        ))} */}
       </ContentLayout>
     </>
   );
