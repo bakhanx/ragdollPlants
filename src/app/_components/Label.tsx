6c836a6f5c5f953e244584ec0cf0185c261a9447
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-07 18:33:45 +0900

    Remove: 파일제거

--- src/app/_components/Label.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Label = ({ text }: { text: string }) => {
-  return (
-    <div className="absolute top-2 right-2 px-2 py-1 bg-[#60b357] text-white text-xs rounded-full">
-      {text}
-    </div>
-  );
-};
