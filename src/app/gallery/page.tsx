bba70f09bfdf7811e4068c1f6aea899d966b4ae5
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-07 18:37:32 +0900

    Feat: 갤러리 컴포넌트

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import BackgroundImage from "../_components/layout/BackgroundImage";
+import ContentLayout from "../_components/layout/ContentsLayout";
+import Header from "../_components/layout/Header";
+import PlantCard from "./_components/PlantCard";
+import FloatingButton from "./_components/FloatingButton";
+
+export default function Page() {
+  return (
+    <>
+      <BackgroundImage src="/images/welcome-bg-06.webp" />
+      {/* Contents */}
+      <ContentLayout>
+        {/* 헤더 */}
+        <Header />
+
+        {/* 식물 카드 부분 */}
+        <PlantCard />
+        
+
+        {/* Floating Button */}
+        <FloatingButton/>
+
+      </ContentLayout>
+    </>
+  );
+}
