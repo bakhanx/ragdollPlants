d31e7a45ad57f027cf073c6761b808f2590372b7
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-11 18:46:36 +0900

    Feat: Footer 적용

--- src/app/_components/layout/ContentsLayout.tsx
+++ src/app/_components/layout/ContentsLayout.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Footer from './Footer';
 
 type ContentLayoutProps = {
   children: React.ReactNode;
   noPadding?: boolean;
+  showFooter?: boolean;
 };
 
 export default function ContentLayout({
@@ -11,8 +13,12 @@ export default function ContentLayout({
 }: ContentLayoutProps) {
   return (
     <div
-      className={`relative z-10 mx-2 flex min-h-[calc(100vh-16px)] my-2 w-full max-w-md flex-col items-center rounded-2xl bg-white/10 shadow-lg backdrop-blur-xl ${noPadding ? '' : 'px-4 py-4'} `}>
-      {children}
+      className={`relative z-10 mx-2 my-2 flex min-h-[calc(100vh-16px)] w-full max-w-md flex-col items-center rounded-2xl bg-white/10 shadow-lg backdrop-blur-xl ${noPadding ? '' : 'px-4 py-4'} `}>
+      <div className="w-full flex-1">{children}</div>
+
+      <div className="mt-auto w-full">
+        <Footer />
+      </div>
     </div>
   );
 }
