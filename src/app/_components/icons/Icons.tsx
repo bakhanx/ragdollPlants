ed16c980bb7ddcddebe984caec794dd0907ab667
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-14 18:55:49 +0900

    Feat: 새로운 Pin 아이콘 추가

--- src/app/_components/icons/Icons.tsx
+++ src/app/_components/icons/Icons.tsx
@@ -2,6 +2,7 @@ import { SVGProps, ComponentType } from 'react';
 import Water from '@/../public/svg/water.svg';
 import Water2 from '@/../public/svg/water2.svg';
 import Nutrient from '@/../public/svg/vape-kit.svg';
+import Pin from '@/../public/svg/pin.svg';
 
 interface IconProps extends SVGProps<SVGSVGElement> {
   size?: number;
@@ -24,3 +25,4 @@ const createIcon = (SvgComponent: ComponentType<SVGProps<SVGSVGElement>>, displa
 export const WaterIcon = createIcon(Water, 'WaterIcon');
 export const Water2Icon = createIcon(Water2, 'Water2Icon');
 export const NutrientIcon = createIcon(Nutrient, 'NutrientIcon');
+export const PinIcon = createIcon(Pin, 'PinIcon');
