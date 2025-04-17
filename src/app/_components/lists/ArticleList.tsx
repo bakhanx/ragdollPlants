e846c20c890c4ea749c80a7dc4a7e83ca9c48aac
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-17 23:48:37 +0900

    Feat: 링크 추가 및 최신 기사 섹션 개선

--- src/app/_components/lists/ArticleList.tsx
+++ src/app/_components/lists/ArticleList.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 type ArticleListProps = {
@@ -12,22 +13,33 @@ type ArticleListProps = {
 
 export const ArticleList = ({ items }: ArticleListProps) => {
   return (
-    <div className="w-full">
-      <div className="py-2 text-lg font-bold">Article</div>
+    <div>
+      <div className="flex items-center justify-between">
+        <h2 className="py-2 text-lg font-bold">최신 기사</h2>
+        <Link
+          href="/articles"
+          className="text-sm text-green-700 hover:text-green-500 hover:underline">
+          더보기
+        </Link>
+      </div>
 
-      <div className="flex w-full max-w-md flex-col gap-4">
-        {items.slice(0, 4).map((plant, index) => (
-          <div
-            key={index}
-            className="relative flex gap-x-2 rounded-md bg-[#ffffffa5] shadow-xl">
-            <div className="flex w-full items-center justify-between px-3">
-              <h2 className="cursor-pointer truncate py-2 font-semibold underline hover:text-green-600">
-                {plant.title}
-              </h2>
-              <div className="text-sm text-gray-600">{plant.date}</div>
-            </div>
-          </div>
-        ))}
+      <div className="w-full">
+        <div className="flex w-full max-w-md flex-col gap-4">
+          {items.slice(0, 4).map((plant) => (
+            <Link
+              href={`/articles/${plant.id}`}
+              key={plant.id}>
+              <div className="relative flex gap-x-2 rounded-md bg-[#ffffffa5] shadow-xl hover:text-green-600">
+                <div className="flex w-full items-center justify-between px-3">
+                  <h2 className="cursor-pointer truncate py-2 font-semibold underline">
+                    {plant.title}
+                  </h2>
+                  <div className="text-sm text-gray-600">{plant.date}</div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
