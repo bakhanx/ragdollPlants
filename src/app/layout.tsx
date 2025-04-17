801af0005b0ebb731469126f92b24919278bbf2e
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-17 23:30:41 +0900

    Feat: 내비게이션 스크롤 관리 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/layout/Navbar";
+import { NavigationScrollManager } from "./_components/utils/navigationScrollManager";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <main className="relative flex min-h-screen items-center justify-center">
           {children}
         </main>
+        <NavigationScrollManager/>
       </body>
     </html>
   );
