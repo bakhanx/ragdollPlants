864c4cf4c5ab5ba465a80274300e7085045f4dcf
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-18 19:04:22 +0900

    Feat: 페이지 상단으로 이동 버튼 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,39 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "./_components/layout/Navbar";
-import { NavigationScrollManager } from "./_components/utils/navigationScrollManager";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { NavigationScrollManager } from './_components/utils/navigationScrollManager';
+import ScrollToTopButton from './_components/common/ScrollToTopButton';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin']
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin']
 });
 
 export const metadata: Metadata = {
-  title: "RagdollPlants - 식물 관리의 모든 것",
-  description: "당신의 식물을 더 건강하게 관리할 수 있는 서비스",
+  title: 'RagdollPlants - 식물 관리의 모든 것',
+  description: '당신의 식물을 더 건강하게 관리할 수 있는 서비스'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html
+      lang="ko"
+      className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="">
-        <Navbar />
         <main className="relative flex min-h-screen items-center justify-center">
           {children}
         </main>
-        <NavigationScrollManager/>
+        <ScrollToTopButton />
+        <NavigationScrollManager />
       </body>
     </html>
   );
