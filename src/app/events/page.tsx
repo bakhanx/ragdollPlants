3188caff250f132a03564d9dd6a9bd15b1f58ce5
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-18 19:11:11 +0900

    Feat: 종료이벤트 목록 추가 및 리팩토링

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,53 +2,31 @@ import React from 'react';
 import BackgroundImage from '../_components/layout/BackgroundImage';
 import ContentLayout from '../_components/layout/ContentsLayout';
 import Header from '../_components/layout/Header';
-import Link from 'next/link';
-import Image from 'next/image';
-import { bannerItems } from '@/app/_constants/eventData';
+import { bannerItems, endedEvents } from '@/app/_constants/eventData';
+import { EventList } from './_components';
 
 export default function EventsPage() {
   return (
     <>
-      <BackgroundImage
-        src="/images/welcome-bg-05.webp"
-        overlay={true}
-      />
+      <BackgroundImage src="/images/welcome-bg-05.webp" />
       <ContentLayout>
         <Header
           title="이벤트"
-          showMenuButton
           showNotification
         />
 
-        <div className="mx-auto w-full max-w-md py-4">
-          <h1 className="mb-4 text-xl font-bold">진행 중인 이벤트</h1>
+        <div className="mx-auto flex w-full max-w-md flex-col gap-y-8 py-4">
+          {/* 진행 중인 이벤트 */}
+          <EventList
+            title="진행 중인 이벤트"
+            events={bannerItems}
+          />
 
-          <div className="space-y-4">
-            {bannerItems.map(event => (
-              <Link
-                key={event.id}
-                href={event.link}
-                className="block">
-                <div className="relative h-40 w-full overflow-hidden rounded-lg transition-shadow hover:shadow-lg">
-                  <Image
-                    src={event.imageUrl}
-                    alt={event.title}
-                    fill
-                    className="object-cover"
-                  />
-
-                  {/* 오버레이 */}
-                  <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent" />
-
-                  {/* 이벤트 정보 */}
-                  <div className="absolute bottom-0 left-0 z-10 p-4 text-white">
-                    <p className="text-sm opacity-80">{event.subtitle}</p>
-                    <h2 className="text-lg font-bold">{event.title}</h2>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
+          {/* 종료된 이벤트 */}
+          <EventList
+            title="종료된 이벤트"
+            events={endedEvents}
+          />
         </div>
       </ContentLayout>
     </>
