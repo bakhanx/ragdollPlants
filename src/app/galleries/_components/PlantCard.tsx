7d74419a7cc18bab900df03bb102c9f777b4a0e3
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-22 20:16:06 +0900

    Refactor: PlantCard 컴포넌트 코드 정리 및 스타일 수정

--- src/app/galleries/_components/PlantCard.tsx
+++ src/app/galleries/_components/PlantCard.tsx
@@ -1,29 +1,28 @@
-import { myPlants } from "@/app/_temp";
-import Image from "next/image";
-import React from "react";
-import Water from "@/../public/svg/water2.svg";
+import { myPlants } from '@/app/_temp';
+import Image from 'next/image';
+import React from 'react';
+import Water from '@/../public/svg/water2.svg';
 
 const PlantCard = () => {
   return (
-    <div className="grid grid-cols-3 justify-center gap-5 w-full h-full py-4">
-        {/* inext 고칠것 */}
+    <div className="grid h-full w-full grid-cols-3 justify-center gap-5 py-4">
+      {/* inext 고칠것 */}
       {myPlants.map((plant, index) => (
         <div
           key={index}
-          className="w-full aspect-square rounded-md relative hover:scale-105 transition-all duration-300 "
-        >
+          className="relative aspect-square w-full rounded-md transition-all duration-300 hover:scale-105">
           <Image
             className="rounded-md"
             fill
             alt={plant.name}
             src={plant.image}
-            style={{ objectFit: "cover" }}
+            style={{ objectFit: 'cover' }}
           />
 
           {/* 상태 아이콘 */}
           {plant.status && (
-            <div className="absolute -top-2 -right-2 size-8 flex items-center justify-center bg-[#ff735d] rounded-full shadow-lg">
-              <Water className="p-2 w-full h-full" />
+            <div className="absolute -top-2 -right-2 flex size-8 items-center justify-center rounded-full bg-[#ff735d] shadow-lg">
+              <Water className="h-full w-full p-2 [&_path]:fill-blue-500" />
             </div>
           )}
         </div>
