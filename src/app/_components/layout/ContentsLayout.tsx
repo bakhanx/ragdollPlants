bbcbcb3318e927ed0d1c1d2e66e49b5aeeb8ead8
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-22 20:11:05 +0900

    Style:  배경색 좀 더 어둡게

--- src/app/_components/layout/ContentsLayout.tsx
+++ src/app/_components/layout/ContentsLayout.tsx
@@ -13,7 +13,7 @@ export default function ContentLayout({
 }: ContentLayoutProps) {
   return (
     <div
-      className={`relative z-10 mx-2 my-2 flex min-h-[calc(100vh-16px)] w-full max-w-md flex-col items-center rounded-2xl bg-white/10 shadow-lg backdrop-blur-xl ${noPadding ? '' : 'px-4 py-4'} `}>
+      className={`relative z-10 mx-2 my-2 flex min-h-[calc(100vh-16px)] w-full max-w-md flex-col items-center rounded-2xl bg-black/10 shadow-lg backdrop-blur-xl ${noPadding ? '' : 'px-4 py-4'} `}>
       <div className="w-full flex-1">{children}</div>
 
       <div className="mt-auto w-full">
