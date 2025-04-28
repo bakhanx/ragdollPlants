0329b0033dfdad851c1158b594e038338fb5ff78
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-28 22:06:09 +0900

    Feat: 이벤트 목록 컴포넌트 통합

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,37 +1,19 @@
-import React from 'react';
 import BackgroundImage from '../_components/layout/BackgroundImage';
 import ContentLayout from '../_components/layout/ContentsLayout';
 import Header from '../_components/layout/Header';
-import { bannerItems, endedEvents } from '@/app/_constants/eventData';
-import { EventList } from './_components';
 import FloatingButton from '@/app/_components/common/UploadButton';
+import { bannerItems, endedEvents } from '@/app/_constants/eventData';
+import EventList from './_components/EventList';
 
 export default function EventsPage() {
   return (
     <>
       <BackgroundImage src="/images/welcome-bg-05.webp" />
       <ContentLayout>
-        <Header
-          title="이벤트"
-          showNotification
-        />
-
-        <div className="mx-auto flex w-full max-w-md flex-col gap-y-8 py-4">
-          {/* 진행 중인 이벤트 */}
-          <EventList
-            title="진행 중인 이벤트"
-            events={bannerItems}
-          />
-
-          {/* 종료된 이벤트 */}
-          <EventList
-            title="종료된 이벤트"
-            events={endedEvents}
-          />
-        </div>
+        <Header title="이벤트" showNotification />
+        <EventList initialActiveEvents={bannerItems} initialEndedEvents={endedEvents} />
       </ContentLayout>
-      
-      <FloatingButton link="/events/create" adminOnly={true} />  
+      <FloatingButton link="/events/create" adminOnly={true} />
     </>
   );
 }
