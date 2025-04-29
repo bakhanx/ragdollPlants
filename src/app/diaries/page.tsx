35fa93cea99a0e4db4a7d946e0e835cb015f4868
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-29 20:51:01 +0900

    Feat: 리팩토링 및 작성하기 버튼 추가

--- src/app/diaries/page.tsx
+++ src/app/diaries/page.tsx
@@ -1,30 +1,30 @@
 import BackgroundImage from '../_components/layout/BackgroundImage';
 import ContentLayout from '../_components/layout/ContentsLayout';
 import Header from '../_components/layout/Header';
-import { diaryPosts } from '../_temp';
+import Link from 'next/link';
 import FloatingButton from '@/app/_components/common/UploadButton';
+import { diaryPosts } from '../_temp/diaryData';
 import DiaryList from './_components/DiaryList';
 
-
-export default function Page() {
+export default function DiariesPage() {
   return (
     <>
       <BackgroundImage src="/images/welcome-bg-06.webp" />
-      {/* Contents */}
       <ContentLayout>
-        {/* 헤더 */}
-        <Header
-          title="식물 일기"
-          showNotification
-        />
-
-        {/* 일기 목록 */}
-        <div className="py-8">
-          <DiaryList posts={diaryPosts} />
+        <Header title="식물 일기" showNotification />
+        <div className="mt-6 flex justify-between items-center">
+          <div className="max-w-xs" />
+          <div>
+            <Link href="/diaries/upload">
+              <button className="rounded-md bg-green-600 px-3 py-1 text-xs text-white hover:bg-green-700">
+                작성하기
+              </button>
+            </Link>
+          </div>
         </div>
+        <DiaryList initialPosts={diaryPosts} />
       </ContentLayout>
-
-      <FloatingButton link="/diaries/create" />
+      <FloatingButton link="/diaries/upload" adminOnly={true} />
     </>
   );
 }
