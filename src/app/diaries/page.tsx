ae702663c263d116609daa367f8607678b3728a7
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-30 21:50:05 +0900

    Refactor: 작성버튼 제거

--- src/app/diaries/page.tsx
+++ src/app/diaries/page.tsx
@@ -1,8 +1,6 @@
 import BackgroundImage from '../_components/layout/BackgroundImage';
-import ContentLayout from '../_components/layout/ContentsLayout';
-import Header from '../_components/layout/Header';
-import Link from 'next/link';
-import FloatingButton from '@/app/_components/common/UploadButton';
+import { ContentsLayout } from '../_components/layout/ContentsLayout';
+import { Header } from '../_components/header/Header';
 import { diaryPosts } from '../_temp/diaryData';
 import DiaryList from './_components/DiaryList';
 
@@ -10,21 +8,13 @@ export default function DiariesPage() {
   return (
     <>
       <BackgroundImage src="/images/welcome-bg-06.webp" />
-      <ContentLayout>
-        <Header title="식물 일기" showNotification />
-        <div className="mt-6 flex justify-between items-center">
-          <div className="max-w-xs" />
-          <div>
-            <Link href="/diaries/upload">
-              <button className="rounded-md bg-green-600 px-3 py-1 text-xs text-white hover:bg-green-700">
-                작성하기
-              </button>
-            </Link>
-          </div>
-        </div>
+      <ContentsLayout>
+        <Header
+          title="식물 일기"
+          showNotification
+        />
         <DiaryList initialPosts={diaryPosts} />
-      </ContentLayout>
-      <FloatingButton link="/diaries/upload" adminOnly={true} />
+      </ContentsLayout>
     </>
   );
 }
