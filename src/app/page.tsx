253722653dbfbf0cac1c80c4e4f1f067abb937c4
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-30 21:46:47 +0900

    Refactor: 컴포넌트 경로 및 import 방식 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import ContentLayout from './_components/layout/ContentsLayout';
+import { ContentsLayout } from './_components/layout/ContentsLayout';
 import BackgroundImage from './_components/layout/BackgroundImage';
-import Banner from './_components/layout/Banner';
+import Banner from './_components/banner/Banner';
 import { MenuList } from './_components/lists/MenuList';
-import Header from './_components/layout/Header';
+import { Header } from './_components/header/Header';
 import { ArticleList } from './_components/lists/ArticleList';
 import { articleItems } from './_temp/articleData';
-import Link from 'next/link';
 
 export default function Page() {
   // 최신 3개의 아티클만 선택
@@ -26,7 +25,7 @@ export default function Page() {
       {/* 배경이미지 */}
       <BackgroundImage src="/images/welcome-bg-07.webp" />
 
-      <ContentLayout>
+      <ContentsLayout>
         {/* 헤더 */}
         <Header showNotification />
 
@@ -38,7 +37,7 @@ export default function Page() {
 
         {/* 기사 리스트 - 최신 3개 표시 */}
         <ArticleList items={latestArticles} />
-      </ContentLayout>
+      </ContentsLayout>
     </>
   );
 }
