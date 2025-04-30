866e2b4ba402ae1adcd53f1c56787d81c1993690
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-04-30 21:49:05 +0900

    Refactor: Footer 조건부 렌더링 추가

--- src/app/_components/layout/ContentsLayout.tsx
+++ src/app/_components/layout/ContentsLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from './Footer';
+import { Footer } from './Footer';
 
 type ContentLayoutProps = {
   children: React.ReactNode;
@@ -7,17 +7,18 @@ type ContentLayoutProps = {
   showFooter?: boolean;
 };
 
-export default function ContentLayout({
+export const ContentsLayout = ({
   children,
-  noPadding = false
-}: ContentLayoutProps) {
+  noPadding = false,
+  showFooter = true
+}: ContentLayoutProps) => {
   return (
     <div
-      className={`relative z-10 mx-2 my-2 flex min-h-[calc(100vh-16px)] w-full max-w-md flex-col items-center rounded-2xl bg-black/10 shadow-lg backdrop-blur-xl ${noPadding ? '' : 'px-4 py-4'} `}>
+className={`relative z-10 mx-2 my-2 flex min-h-[calc(100vh-16px)] w-full max-w-md flex-col items-center rounded-2xl bg-black/10 shadow-lg backdrop-blur-xl ${noPadding ? '' : 'px-4 py-4'} `}>
       <div className="w-full flex-1">{children}</div>
 
       <div className="mt-auto w-full">
-        <Footer />
+        {showFooter && <Footer />}
       </div>
     </div>
   );
