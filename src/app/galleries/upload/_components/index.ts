57441a699cb81ef157c2fdd1a9921f4452b4577f
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-05-16 18:37:23 +0900

    Feat: 통합 및 내보내기

---
+++ src/app/galleries/upload/_components/index.ts
@@ -0,0 +1,5 @@
+export { default as GalleryUploadForm } from './GalleryUploadForm';
+export { default as GalleryUsageInfo } from './GalleryUsageInfo';
+export { default as GalleryPreview } from './GalleryPreview';
+export { default as GalleryTitleField } from './GalleryTitleField';
+export { default as SubmitButton } from './GalleryUploadButton'; 
