cc922f53f488e7968932c228eba4d18dda1e24bc
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-05-23 21:52:09 +0900

    Feat: 배너 슬라이드 컴포넌트

---
+++ src/app/_components/banner/BannerSlide.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+type BannerSlideProps = {
+  id: string | number;
+  title: string;
+  subtitle: string;
+  imageUrl: string;
+  link: string;
+  isActive: boolean;
+};
+
+export const BannerSlide: React.FC<BannerSlideProps> = ({
+  id,
+  title,
+  subtitle,
+  imageUrl,
+  link,
+  isActive
+}) => {
+  return (
+    <Link
+      href={link}
+      className="absolute top-0 left-0 h-full w-full transition-opacity duration-500"
+      style={{
+        opacity: isActive ? 1 : 0,
+        zIndex: isActive ? 10 : 0,
+        pointerEvents: isActive ? 'auto' : 'none'
+      }}>
+      {/* 배경 이미지 */}
+      <div className="relative h-full w-full">
+        <Image
+          src={imageUrl}
+          alt={title}
+          fill
+          style={{ objectFit: 'cover' }}
+          className="rounded-xl"
+          priority
+        />
+        {/* 어두운 오버레이 */}
+        <div className="absolute inset-0 rounded-xl bg-black opacity-30" />
+      </div>
+
+      {/* 텍스트 내용 */}
+      <div className="absolute bottom-4 left-4 z-20">
+        <p className="text-xs sm:text-sm">{subtitle}</p>
+        <h2 className="text-lg leading-snug font-medium sm:text-xl">
+          {title}
+        </h2>
+      </div>
+    </Link>
+  );
+};
+
+export default BannerSlide; 
