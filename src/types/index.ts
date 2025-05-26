32f88c518fa3bd028776d4fc9f3d0655c797cb21
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-05-26 22:33:06 +0900

    Feat: 타입 정의 인덱스

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 모든 타입 정의를 내보내는 인덱스 파일
+ */
+
+// 모델 타입 내보내기
+export * from './models/article';
+
+// 컴포넌트 타입 내보내기
+export * from './components/articles';
+
+// 서비스 타입 내보내기
+export * from './services/articleService'; 
