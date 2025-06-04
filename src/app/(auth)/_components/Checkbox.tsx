b3172a7817550b2831a831312c1cd3adb8876abc
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-06-04 18:33:07 +0900

    Feat: ref 전달 및 에러 메시지 추가

--- src/app/(auth)/_components/Checkbox.tsx
+++ src/app/(auth)/_components/Checkbox.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { forwardRef } from 'react';
 
 type CheckboxProps = {
   label: string;
-};
+  error?: string;
+  name: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Checkbox = ({ label }: CheckboxProps) => {
-  return (
-    <div className="flex items-center gap-2">
-      <input type="checkbox" className="w-5 h-5 " />
-      <span className="text-sm text-gray-200">{label}</span>
-    </div>
-  );
-};
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ label, error, name, className = '', ...props }, ref) => {
+    return (
+      <div className="w-full">
+        <div className="flex items-center gap-3">
+          <input
+            ref={ref}
+            name={name}
+            type="checkbox"
+            className={`h-5 w-5 rounded border-gray-300 ${className}`}
+            {...props}
+          />
+          <label className="cursor-pointer text-sm text-white">{label}</label>
+        </div>
+        {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
+      </div>
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
