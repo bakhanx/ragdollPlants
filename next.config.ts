8aa883022dcf9b1d595986add3c97552ebf6e067
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-06-25 20:17:06 +0900

    Feat: cf images 원격 패턴 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
