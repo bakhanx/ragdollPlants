30a1c2c46ff1cb646ce420846b70f6bf253a66e0
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-06-25 20:19:56 +0900

    Refactor: 'use client' 제거

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { ContentsLayout } from '../../_components/layout/ContentsLayout';
 import BackgroundImage from '../../_components/layout/BackgroundImage';
