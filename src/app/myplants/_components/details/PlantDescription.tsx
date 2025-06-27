45c21ab34a045bd79f518a97fadc732c03e7eb8b
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-06-27 21:52:45 +0900

    Feat: 식물 설명 컴포넌트

---
+++ src/app/myplants/_components/details/PlantDescription.tsx
@@ -0,0 +1,16 @@
+interface PlantDescriptionProps {
+  description?: string;
+}
+
+export const PlantDescription = ({ description }: PlantDescriptionProps) => {
+  if (!description) return null;
+
+  return (
+    <div className="py-4">
+      <h2 className="mb-3 text-lg font-semibold text-gray-50">메모</h2>
+      <p className="min-h-32 rounded-lg border border-gray-50 p-3 text-gray-50">
+        {description}
+      </p>
+    </div>
+  );
+};
