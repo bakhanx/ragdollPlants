ccdd125ea5c1ac582bd0da51b1910e3fec3bb288
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-07-06 20:15:09 +0900

    Style: 갤러리 제목 섹션의 텍스트 크기 조정 및 스타일 개선

--- src/app/galleries/_components/GalleryTitleSection.tsx
+++ src/app/galleries/_components/GalleryTitleSection.tsx
@@ -8,8 +8,8 @@ export const GalleryTitleSection = ({ photoCount }: GalleryTitleSectionProps) =>
     <div className="mt-16 w-full p-4">
       <div className="relative flex flex-col items-center">
         <div className="text-center text-white">
-          <h1 className="mb-1 text-3xl font-bold">My Plant Gallery</h1>
-          <p className="mb-8 text-sm opacity-80">
+          <h1 className="mb-1 sm:text-3xl text-2xl font-bold">My Plant Gallery</h1>
+          <p className="mb-8 sm:text-sm text-xs opacity-80">
             내 소중한 식물들의 아름다운 순간들
           </p>
           <div className="mx-auto mb-8 h-px w-16 bg-white opacity-30"></div>
