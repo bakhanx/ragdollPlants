5bf6927cb45640b5e744fa5ee99edab408890af0
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-07-11 22:34:25 +0900

    Refactor: 아이콘 export 간소화

--- src/app/_components/icons/index.ts
+++ src/app/_components/icons/index.ts
@@ -1,16 +1,1 @@
-export {
-  WaterIcon,
-  Water2Icon,
-  NutrientIcon,
-  PinIcon,
-  EditIcon,
-  BackIcon,
-  CloseIcon,
-  MenuIcon,
-  HamburgerIcon,
-  BellIcon,
-  ShareIcon,
-  ImageIcon,
-  SearchIcon,
-  SignOutIcon
-} from './Icons';
+export * from './Icons';
