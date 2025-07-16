4480ee2ffdd294c61268f8a3e5313766ee823aef
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-07-16 23:57:40 +0900

    Refactor: 상태 관리 및 메뉴 사이드바 추가

--- src/app/_components/header/HeaderLeft.tsx
+++ src/app/_components/header/HeaderLeft.tsx
@@ -1,24 +1,39 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { BackIcon, HamburgerIcon } from '@/app/_components/icons';
+import { useRouter } from 'next/navigation';
+import { MenuList } from '../lists/MenuList';
+import { MenuSidebar } from './MenuSidebar';
 
 interface HeaderLeftProps {
-  showBack: boolean;
-  onBackClick: () => void;
-  onMenuOpen: () => void;
+  showBack?: boolean;
 }
 
-export const HeaderLeft = ({
-  showBack,
-  onBackClick,
-  onMenuOpen
-}: HeaderLeftProps) => {
+export const HeaderLeft = ({ showBack }: HeaderLeftProps) => {
+  const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleBackClick = () => {
+    router.back();
+  };
+
+  const handleOpenMenu = () => {
+    setIsMenuOpen(true);
+  };
+
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className="w-10">
+      <MenuSidebar
+        isOpen={isMenuOpen}
+        onClose={handleCloseMenu}
+      />
       {showBack ? (
         <button
-          onClick={onBackClick}
+          onClick={handleBackClick}
           className="group flex size-9 items-center justify-center rounded-xl bg-white/50 transition-all hover:bg-white/70 hover:shadow-md"
           aria-label="뒤로 가기">
           <BackIcon
@@ -28,7 +43,7 @@ export const HeaderLeft = ({
         </button>
       ) : (
         <button
-          onClick={onMenuOpen}
+          onClick={handleOpenMenu}
           className="group flex size-9 items-center justify-center rounded-xl bg-white/50 transition-all hover:bg-white/70 hover:shadow-md"
           aria-label="네비게이션 메뉴 열기">
           <HamburgerIcon
@@ -39,4 +54,4 @@ export const HeaderLeft = ({
       )}
     </div>
   );
-}
+};
