73b9dd8e411ad9da9be8a7c68a05426f2451b9da
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-07-29 17:38:43 +0900

    Refactor: 각 모듈 인터페이스에 loginId 추가

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,36 +1,40 @@
-import NextAuth from "next-auth"
+import NextAuth from 'next-auth';
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
     user: {
-      id: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-      role: string
-    }
+      id: string;
+      loginId: string;
+      name: string;
+      email?: string | null;
+      image?: string | null;
+      role: string;
+    };
   }
 
   interface User {
-    id: string
-    name?: string | null
-    email: string
-    image?: string | null
-    role: string
-    isActive: boolean
+    id: string;
+    loginId: string;
+    name: string;
+    email: string;
+    image?: string | null;
+    role: string;
+    isActive: boolean;
   }
 }
 
-declare module "next-auth/jwt" {
+declare module 'next-auth/jwt' {
   interface JWT {
-    id: string
-    role: string
+    id: string;
+    loginId: string;
+    role: string;
   }
 }
 
-declare module "@auth/core/adapters" {
+declare module '@auth/core/adapters' {
   interface AdapterUser {
-    role: string
-    isActive: boolean
+    loginId: string;
+    role: string;
+    isActive: boolean;
   }
-} 
+}
