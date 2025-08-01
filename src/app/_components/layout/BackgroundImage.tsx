94b9c72fcb0410cc964c3d7c06d28aa6975aa92e
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-08-01 16:52:59 +0900

    Refactor: 배경이미지 처리 변경 Next/Image -> CSS

--- src/app/_components/layout/BackgroundImage.tsx
+++ src/app/_components/layout/BackgroundImage.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 import React from 'react';
 
 type BackgroundImageProps = {
@@ -6,19 +6,31 @@ type BackgroundImageProps = {
   className?: string;
 };
 
-export const BackgroundImage = ({ src, className = '' }: BackgroundImageProps) => {
+export const BackgroundImage = ({
+  src,
+  className = ''
+}: BackgroundImageProps) => {
+  // CSS
   return (
-    <div className={`fixed left-0 top-0 -z-10 h-full w-full overflow-hidden ${className}`}>
-      <Image
-        src={src}
-        alt="배경 이미지"
-        fill
-        className="object-cover object-center"
-        priority
-        sizes="100vw"
-      />
-    </div>
+    <div
+      className={`fixed top-0 left-0 -z-10 h-full w-full bg-cover bg-center ${className}`}
+      style={{ backgroundImage: `url(${src})` }}
+    />
   );
+
+  // Next/Image
+  // return (
+  //   <div className={`fixed left-0 top-0 -z-10 h-full w-full overflow-hidden ${className}`}>
+  //     <Image
+  //       src={src}
+  //       alt="배경 이미지"
+  //       fill
+  //       className="object-cover object-center"
+  //       priority
+  //       sizes="100vw"
+  //     />
+  //   </div>
+  // );
 };
 
 export default BackgroundImage;
