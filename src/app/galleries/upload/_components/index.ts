f353a8244044dcabcb6a4fd826339869354b43fd
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-08-12 22:22:02 +0900

    Refactor: 갤러리제목필드 제거

--- src/app/galleries/upload/_components/index.ts
+++ src/app/galleries/upload/_components/index.ts
@@ -1,5 +1,4 @@
 export { default as GalleryUploadForm } from './GalleryUploadForm';
 export { default as GalleryUsageInfo } from './GalleryUsageInfo';
 export { default as GalleryPreview } from './GalleryPreview';
-export { default as GalleryTitleField } from './GalleryTitleField';
-export { default as SubmitButton } from './GalleryUploadButton'; 
+export { default as SubmitButton } from './GalleryUploadButton';
