3878e98dc5fdc0f19ebea13d95632fa2fd4d75ce
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-09-20 23:02:39 +0900

    Feat: 로그인 아이콘 추가

--- src/app/_components/icons/Icons.tsx
+++ src/app/_components/icons/Icons.tsx
@@ -410,6 +410,27 @@ export const SearchIcon = ({ size = 16, className = '' }: IconProps) => (
   </svg>
 );
 
+// LogIn Icon
+export const LogInIcon = ({ size = 16, className = '' }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className={className}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+    />
+  </svg>
+);
+
+
+
 // SignOut Icon
 export const SignOutIcon = ({ size = 16, className = '' }: IconProps) => (
   <svg
@@ -493,6 +514,7 @@ export const Icons = {
   Schedule: ScheduleIcon,
   Image: ImageIcon,
   Search: SearchIcon,
+  login: LogInIcon,
   SignOut: SignOutIcon,
   Heart: HeartIcon,
   Article: ArticleIcon
