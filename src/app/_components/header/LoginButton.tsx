60ec453280e8e69b59b869f57ae888e135f73a73
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-09-20 23:03:24 +0900

    Refactor: 로그인 버튼 아이콘으로 변경

--- src/app/_components/header/LoginButton.tsx
+++ src/app/_components/header/LoginButton.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import Link from 'next/link';
+import { LogInIcon } from '@/app/_components/icons';
 
 export const LoginButton = () => {
   return (
     <Link
       href="/login"
-      className="flex h-9 items-center justify-center rounded-xl bg-green-600 px-4 text-sm font-medium text-white transition-all hover:bg-green-700 hover:shadow-md"
+      className="flex h-9 items-center justify-center rounded-xl bg-green-700 px-2 text-sm font-medium text-white transition-all hover:bg-green-800 hover:shadow-md"
+      title="로그인"
     >
-      로그인
+      <LogInIcon size={20} className="text-white" />
     </Link>
   );
 };
