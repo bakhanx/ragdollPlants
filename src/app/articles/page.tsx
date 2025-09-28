d938066af7ab6a6dac9e0e09a1e3d925c4d7c43f
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-09-28 23:13:02 +0900

    Refactor: admin 체크 제거

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -5,10 +5,7 @@ import BackgroundImage from '@/app/_components/layout/BackgroundImage';
 import ArticleListWrapper from './_components/ArticleListWrapper';
 import ArticleCardsSkeleton from './_components/ArticleCardsSkeleton';
 
-import { checkIsAdmin } from '@/lib/auth-utils';
 export default async function ArticlesPage() {
-  const isAdmin = await checkIsAdmin();
-
   return (
     <>
       <BackgroundImage src="/images/welcome-bg-07.webp" />
@@ -16,11 +13,6 @@ export default async function ArticlesPage() {
         <Header title="아티클" />
 
         <div className="w-full py-4">
-          {/* 업로드 버튼 */}
-          {/* <div className="mt-4 mb-6 flex justify-end">
-            <AdminUploadButton isAdmin={isAdmin} />
-          </div> */}
-
           {/* 아티클 목록  */}
           <Suspense fallback={<ArticleCardsSkeleton />}>
             <ArticleListWrapper />
