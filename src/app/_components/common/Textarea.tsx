b5a74fd7f77d4ad7cf46df92e06b3aa825a8e3e0
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-09-28 23:05:15 +0900

    Style: 텍스트 색상 변경

--- src/app/_components/common/Textarea.tsx
+++ src/app/_components/common/Textarea.tsx
@@ -26,7 +26,7 @@ export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
     },
     ref
   ) => {
-    const textareaStyles = `bg-opacity-20 w-full rounded-md border border-gray-300 px-3 py-2 text-sm  focus:border-green-500 focus:ring-1 focus:ring-green-500 focus:outline-none resize-none ${
+    const textareaStyles = `bg-opacity-20 w-full rounded-md border border-gray-300 px-3 py-2 text-sm text-white  focus:border-green-500 focus:ring-1 focus:ring-green-500 focus:outline-none resize-none ${
       error ? 'border-red-500 focus:border-red-500 focus:ring-red-500' : ''
     } ${className}`;
 
