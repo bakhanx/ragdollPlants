c57895fa456d1e4962c1d201f36de3ad934d1a1f
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-10-01 23:52:50 +0900

    Style: 줄바꿈 공백 처리

--- src/app/myplants/_components/details/PlantDescription.tsx
+++ src/app/myplants/_components/details/PlantDescription.tsx
@@ -8,7 +8,7 @@ export const PlantDescription = ({ description }: PlantDescriptionProps) => {
   return (
     <div className="py-4">
       <h2 className="mb-3 text-lg font-semibold text-gray-50">메모</h2>
-      <p className="min-h-32 rounded-lg border border-gray-50 p-3 text-gray-50">
+      <p className="min-h-32 rounded-lg border-gray-50 p-3 text-gray-50 whitespace-pre-wrap">
         {description}
       </p>
     </div>
