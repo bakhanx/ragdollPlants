e6d996627e20244c9328736549f5ecb2818f98e7
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-10-02 21:56:15 +0900

    Feat: 기본 카테고리 종류 추가

--- src/app/_constants/plantData.ts
+++ src/app/_constants/plantData.ts
@@ -1,16 +1,22 @@
-import { PlantCategory } from '@/types/models/plant';
-
 /**
  * 식물 카테고리 옵션
  */
-export const PLANT_CATEGORIES: PlantCategory[] = [
-  '실내식물',
-  '다육식물', 
+export const PLANT_CATEGORIES = [
+  '공기정화',
+  '관엽',
+  '야자',
+  '수경',
+  '다육',
   '선인장',
-  '관엽식물',
   '허브',
+  '꽃',
+  '야생화',
+  '리톱스',
+  '구근',
+  '채소',
+  '과일',
   '기타'
-];
+] as const;
 
 /**
  * 식물 카테고리를 select 옵션 형태로 변환
@@ -28,4 +34,4 @@ export const DEFAULT_WATERING_INTERVAL = 7;
 /**
  * 기본 영양제 간격 (일)
  */
-export const DEFAULT_NUTRIENT_INTERVAL = 30; 
+export const DEFAULT_NUTRIENT_INTERVAL = 30;
