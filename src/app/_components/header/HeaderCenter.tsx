f943b38a88327a7a0ed85a3e101319c44ef83457
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-10-02 21:55:03 +0900

    Style: 헤더 로고 및 텍스트 위치 변경

--- src/app/_components/header/HeaderCenter.tsx
+++ src/app/_components/header/HeaderCenter.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 interface HeaderCenterProps {
-  title: string;
   logoSrc: string;
 }
 
@@ -11,25 +10,26 @@ interface HeaderCenterProps {
  * 헤더의 중앙 섹션 컴포넌트
  * 로고와 타이틀을 표시
  */
-export const HeaderCenter = ({ title, logoSrc }: HeaderCenterProps) => {
+export const HeaderCenter = ({ logoSrc }: HeaderCenterProps) => {
   return (
     <div className="pointer-events-none absolute right-0 left-0 mx-auto flex items-center justify-center">
       <Link
         href="/"
-        className="pointer-events-auto flex max-w-[45%] items-center gap-x-2 sm:max-w-[50%]">
-        <div className="relative size-8 flex-shrink-0">
-          <Image
-            className="rounded-full"
-            alt="Logo"
-            src={`${logoSrc}`}
-            style={{ objectFit: 'cover' }}
-            width={32}
-            height={32}
-          />
-        </div>
-        <h1 className="truncate text-lg leading-tight font-semibold text-gray-100">
-          {title}
-        </h1>
+        className="pointer-events-auto max-w-[45%] items-center gap-x-2 sm:max-w-[50%]">
+        <span className="flex items-center gap-x-1 truncate text-lg leading-tight font-semibold text-gray-100">
+          <span>Ragdoll</span>
+          <div className="size-8 flex-shrink-0">
+            <Image
+              className="rounded-full"
+              alt="Logo"
+              src={`${logoSrc}`}
+              style={{ objectFit: 'cover' }}
+              width={32}
+              height={32}
+            />
+          </div>
+          <span>Plants</span> <span> </span>
+        </span>
       </Link>
     </div>
   );
