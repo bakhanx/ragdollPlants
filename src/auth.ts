0363a7a5a1894361e952f8222aee93020a17ad0b
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-10-14 01:43:44 +0900

    TEST: vercel env 테스트

--- src/auth.ts
+++ src/auth.ts
@@ -96,7 +96,19 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     })
   ],
   session: {
-    strategy: 'jwt'
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  cookies: {
+    sessionToken: {
+      name: `${process.env.NODE_ENV === 'production' ? '__Secure-' : ''}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production'
+      },
+    },
   },
   callbacks: {
     async jwt({ token, user }) {
